bb75ce50174d209027df89d4e8b0cb2958f080d3
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2020-02-03 16:27:59 -0400

    Aula 34

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
 
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
 
@@ -11,6 +13,25 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Enter message</title> </head>');
         res.write('<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit"> Send</button></form> </body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        //on() ==== listen to certain events
+        req.on('data', (chunk) => {
+            console.log('chunk', chunk);
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            console.log('message =', message);
+        })
+        
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
